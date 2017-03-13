4d17526b3c4ab7e91dbddf458334c3d895cccf52
Alex Sanders <a@snd.rs>
2017-03-13 16:13:09 +0000

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    extends: ['airbnb-base', 'prettier'],
+    installedESLint: true,
+    plugins: ['import', 'prettier'],
+    rules: {
+        'no-console': 0,
+        'import/newline-after-import': 0,
+		'no-underscore-dangle': 0,
+        'prettier/prettier': [
+            'error',
+            {
+                trailingComma: 'es5',
+                singleQuote: true,
+                tabWidth: 4,
+                bracketSpacing: true
+            }
+        ]
+    },
+    parserOptions: {
+        ecmaVersion: 2016,
+        sourceType: 'module',
+    },
+    env: {
+        es6: true,
+        node: true,
+        jest: true
+    }
+};
